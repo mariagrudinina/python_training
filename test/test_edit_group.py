2ffab69ee5150ab6fce4ade79faca68487099b70
Maria Grudinina <mary.gru@mail.ru>
2020-03-14 00:01:08 +0600

    Refactored tests to edit groups

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,3 +5,15 @@ def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name='Edited name', header='Edited header', footer='Edited footer'))
     app.session.logout()
+
+
+def test_edit_first_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name='Edited only name'))
+    app.session.logout()
+
+
+def test_edit_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header='Edited only header'))
+    app.session.logout()
