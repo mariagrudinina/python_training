43356559809127def364c28d5c037882b06942b0
Maria Grudinina <mary.gru@mail.ru>
2020-03-15 01:46:18 +0600

    Updated tests to add/edit a group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,28 +1,31 @@
 from model.group import Group
 
 
-def test_edit_first_group(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="Test"))
-    old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name='Edited name', header='Edited header', footer='Edited footer'))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+# def test_edit_first_group(app):
+#    if app.group.count() == 0:
+#        app.group.create(Group(name="Test"))
+#    old_groups = app.group.get_group_list()
+#    app.group.edit_first_group(Group(name='Edited name', header='Edited header', footer='Edited footer'))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Test"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name='Edited only name'))
+    group = Group(name='Edited only name')
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-
-def test_edit_first_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="Test"))
-    old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(header='Edited only header'))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+# def test_edit_first_group_header(app):
+#    if app.group.count() == 0:
+#        app.group.create(Group(name="Test"))
+#    old_groups = app.group.get_group_list()
+#    app.group.edit_first_group(Group(header='Edited only header'))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
