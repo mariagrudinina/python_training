020ad48c4d6dbf2a3ae06e6f5f577312c839face
Maria Grudinina <mary.gru@mail.ru>
2020-03-15 20:07:40 +0600

    Updated group tests to edit/delete random items

--- fixture/group.py
+++ fixture/group.py
@@ -36,19 +36,25 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
-    def delete_first_group(self):
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
+    def delete_first_group(self):
+        self.delete_group_by_index(0)
+
     def edit_first_group(self, group):
+        self.edit_group_by_index(0, group)
+
+    def edit_group_by_index(self, index, group):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit editing
         wd.find_element_by_name("edit").click()
         self.fill_group_form(group)
@@ -60,6 +66,10 @@ class GroupHelper:
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def open_groups_page(self):
         wd = self.app.wd
