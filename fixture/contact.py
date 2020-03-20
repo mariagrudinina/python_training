eebc7de3bf94176c96d438edbff6af864c72598a
Maria Grudinina <mary.gru@mail.ru>
2020-03-20 22:57:48 +0600

    Added tests to verify phone numbers

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.support.ui import Select
 
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -61,7 +62,7 @@ class ContactHelper:
     def edit_contact_by_index(self, index, contact):
         wd = self.app.wd
         self.open_home_page()
-        wd.find_elements_by_xpath("//a[img[@title='Edit']]")[index].click()
+        self.open_contact_to_edit_by_index(index)
         self.fill_contact_form(contact)
         wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
         self.return_to_home_page()
@@ -94,11 +95,51 @@ class ContactHelper:
             wd = self.app.wd
             self.open_home_page()
             self.contact_cache = []
-            i = 1
             for element in wd.find_elements_by_name("entry"):
-                last_name = wd.find_element_by_xpath('((//tr[@name="entry"])/td[2])[' + str(i) + ']').text
-                first_name = wd.find_element_by_xpath('((//tr[@name="entry"])/td[3])[' + str(i) + ']').text
-                contact_id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.contact_cache.append(Contact(firstname=first_name, lastname=last_name, contact_id=contact_id))
-                i += 1
+                cells = element.find_elements_by_tag_name("td")
+                last_name = cells[1].text
+                first_name = cells[2].text
+                contact_id = cells[0].find_element_by_name("selected[]").get_attribute("value")
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(firstname=first_name, lastname=last_name, contact_id=contact_id,
+                                                  home_phone_number=all_phones[0], mobile_phone_number=all_phones[1],
+                                                  work_phone_number=all_phones[2], phone2=all_phones[3]))
         return list(self.contact_cache)
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, contact_id=id, home_phone_number=homephone,
+                       work_phone_number=workphone, mobile_phone_number=mobilephone, phone2=secondaryphone)
+
+    def get_contact_info_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+        return Contact(home_phone_number=homephone, work_phone_number=workphone,
+                       mobile_phone_number=mobilephone, phone2=secondaryphone)
