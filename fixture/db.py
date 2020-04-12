b81441903b70af77483533a558a9f75335bd6400
Maria Grudinina <mary.gru@mail.ru>
2020-04-13 00:03:46 +0600

    Added test for contact info on home page

--- fixture/db.py
+++ fixture/db.py
@@ -37,5 +37,20 @@ class DbFixture:
             cursor.close()
         return list
 
+    def get_contact_list_with_phones_emails_address(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(
+                "select id, firstname, lastname, address, home, mobile, work, email, email2, email3, phone2 from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname, address, home, mobile, work, email, email2, email3, phone2) = row
+                list.append(Contact(contact_id=str(id), firstname=firstname, lastname=lastname, address=address,
+                                    home_phone_number=home, mobile_phone_number=mobile, work_phone_number=work,
+                                    email=email, email2=email2, email3=email3, phone2=phone2))
+        finally:
+            cursor.close()
+        return list
+
     def destroy(self):
         self.connection.close()
--- test/test_phones.py
+++ test/test_phones.py
@@ -28,6 +28,23 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
 
 
+def test_all_info_on_home_page(app, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="Test", work_phone_number="+73812999999", home_phone_number="+12345678",
+                                   email="test1@dev.com", email3="test3@dev.com", address="test address\r\n644000"))
+    contact_from_home_page = app.contact.get_contact_list()
+    contact_from_db = db.get_contact_list_with_phones_emails_address()
+    assert len(contact_from_home_page) == len(contact_from_db)
+    contact_from_home_page = sorted(contact_from_home_page, key=Contact.id_or_max)
+    contact_from_db = sorted(contact_from_db, key=Contact.id_or_max)
+    for i in range(len(contact_from_db)):
+        assert contact_from_home_page[i].all_phones_from_homepage == merge_phones_like_on_home_page(contact_from_db[i])
+        assert contact_from_home_page[i].firstname == contact_from_db[i].firstname
+        assert contact_from_home_page[i].lastname == contact_from_db[i].lastname
+        assert contact_from_home_page[i].address == contact_from_db[i].address
+        assert contact_from_home_page[i].all_emails_from_homepage == merge_emails_like_on_home_page(contact_from_db[i])
+
+
 def clear(s):
     return re.sub("[() -]", "", s)
 
